24af25e367875876b966330f23e765cbbc565880
Ruoris <48207031+Ruoris@users.noreply.github.com>
2020-03-23 12:52:33 +0200

    kokeilu mergee ollille

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject audioSettings;
+    public GameObject mainMenu;
+    public bool changer;
+    public GameObject backButton;
+
+
+
+    public void settingsMenuDisabler()
+    {
+        if (mainMenu.activeSelf == false)
+        {
+            audioSettings.SetActive(false);
+            backButton.SetActive(false);
+           mainMenu.SetActive(true);
+        }
+        else
+        {
+            audioSettings.SetActive(true);
+            backButton.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+    }
+    public void BackButtonActivator()
+    {
+
+    }
+    public void onClick()
+    {
+        if (audioSettings.activeSelf == false)
+        {
+
+            mainMenu.SetActive(false);
+            audioSettings.SetActive(true);
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            audioSettings.SetActive(false);
+        }
+
+
+    }
+}
