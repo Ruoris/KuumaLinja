2faa665b7f2a590d06994c5f8842bed3aaad6695
RoopeJok <roopejok@metropolia.fi>
2020-03-26 18:44:07 +0200

    Door added

---
+++ Assets/Scripts/RoopenScriptit/Door.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject doortrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.tag == "Player")
+        {
+            door.transform.Rotate(0, 0, 90);
+            doortrigger.transform.Rotate(0, 0, 90);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            door.transform.Rotate(0, 0, -90);
+            doortrigger.transform.Rotate(0, 0, -90);
+        }
+    }
+}
