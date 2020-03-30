4530f709e247688db52aded1fb812afeaeafe8d6
Ruoris <48207031+Ruoris@users.noreply.github.com>
2020-03-30 13:06:30 +0300

    pakollinen commit ennen mergee.
    ammocounteria muutettu

--- Assets/Scripts/ToninScriptit/AmmocounterScript.cs
+++ Assets/Scripts/ToninScriptit/AmmocounterScript.cs
@@ -8,14 +8,18 @@ public class AmmocounterScript : MonoBehaviour
 
     public GameObject[] pistolBullets;
     public GameObject Player;
-    public int bulletsLeft=9;
+    private int bulletsLeft;
     void Update()
-    {
+    { bulletsLeft = Player.GetComponent<Weapons>().ammoLeft;
         if (Input.GetButton("Fire1"))
         {
-            ChangeColor();
+            if (bulletsLeft != 0)
+            {
+ ChangeColor();
+            }
+           
         } 
-        bulletsLeft = Player.GetComponent<Weapons>().ammoLeft -1;
+       
     }
     void Start()
     {
@@ -34,8 +38,17 @@ public class AmmocounterScript : MonoBehaviour
         Color used = new Color32(75, 75, 75, 255);
        
         pistolBullets[bulletsLeft].GetComponent<Image>().color = used;
-        bulletsLeft--;
+       
 
 
+    }
+    public void ReturnColor()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Color used = new Color32(255, 255, 255, 255);
+
+            pistolBullets[i].GetComponent<Image>().color = used;
+        }
     }
 }
