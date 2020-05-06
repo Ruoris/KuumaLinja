e7fe84d97097a7457aaaceeee069d1d7cc166bf2
juusopes <36258359+juusopes@users.noreply.github.com>
2020-05-06 12:50:29 +0300

    leveltemplate & elevator update

--- Assets/Scripts/JuusonScriptit/Bullet.cs
+++ Assets/Scripts/JuusonScriptit/Bullet.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public CircleCollider2D bulletRB;
+    private void Start()
+    {
+    }
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Untagged")
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Enemy"))
         {
-            Physics2D.IgnoreCollision(other.collider, other.collider);
+            Destroy(gameObject);
         }
-        //if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Enemy"))
-        //{
-        //    Destroy(gameObject);
-        //}
     }
 }
--- Assets/Scripts/JuusonScriptit/CameraPosition.cs
+++ Assets/Scripts/JuusonScriptit/CameraPosition.cs
@@ -1,24 +1,60 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CameraPosition : MonoBehaviour
+//{
+//    public GameObject player;
+//    public GameObject playerCamera;
+
+//    private void Start()
+//    {
+//        //Instantiate(playerCamera, player.transform.position + new Vector3(0, 0, -10), player.transform.rotation);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        var player = GameObject.FindWithTag("Player");
+//        if(player != null)
+//        {
+//            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+//        }
+//    }
+//}
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraPosition : MonoBehaviour
 {
     public GameObject player;
-    public GameObject playerCamera;
 
-    private void Start()
+    private float xMax, xMin, yMin, yMax;
+    // Update is called once per frame
+
+    void SetCameraValues(int xmax, int xmin, int ymin, int ymax) //kameran minimi/maksimi sallitut koordinaatit
     {
-        //Instantiate(playerCamera, player.transform.position + new Vector3(0, 0, -10), player.transform.rotation);
+        xMax = xmax;
+        xMin = xmin;
+        yMin = ymin;
+        yMax = ymax;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        var player = GameObject.FindWithTag("Player");
-        if(player != null)
+        SetCameraValues(10, 1, 1, 10);
+    }
+    void LateUpdate()
+    {
+
+        var player = GameObject.Find("Player");
+
+        if (player != null)
         {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+            transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, xMin, xMax), Mathf.Clamp(player.transform.position.y, yMin, yMax), -10);
         }
     }
 }
